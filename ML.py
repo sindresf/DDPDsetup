79a49a90fdbef9298a2549aa9e3de61fb5f50e1c
Fjermestad Sindre <Sindre.Fjermestad@norconsult.com>
2019-01-11 13:32:24 +0100

    add comment about shapping it.

--- ML.py
+++ ML.py
@@ -62,5 +62,6 @@ def test(digit: int = 0, shuffle: bool = False):
     plt.yticks(())
     plt.show()
 
+    # TODO: SHAP VALUE THIS SHITT!
     # TODO: rewrite so mlf can log the metrics
     # TODO: mlf to add the artifacts of the plots
